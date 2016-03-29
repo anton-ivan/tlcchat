c30481f828b1b2e9208585fcf1469bd1e74fe8f3
Anton Ivanov <an.ivan0209@gmail.com>
2016-03-29 11:25:09 +0300

    login form controls changed into material

--- Chat/components/Login.js
+++ Chat/components/Login.js
@@ -3,27 +3,19 @@
 var React = require('react-native');
 var {View, Text, Image,StyleSheet} = React;
 var Button = require('react-native-button');
-
-class Login extends React.Component {
-    render(){
-        let Actions = this.props.routes;
-
-        return (
-            <View style={styles.container}>
-                <Image style={styles.logo} source={require('../assets/logo.png')}/>
-                <Button onPress={Actions.pop}>Login</Button>
-            </View>
-        );
-    }
-}
+var TextFields = require('./textfields');
+import {
+  MKTextField,
+  MKColor,
+  mdl,
+} from 'react-native-material-kit';
 
 var styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-
         alignItems: 'center',
-        backgroundColor: '#1A8FC8',
+        backgroundColor: '#FFFFFF',
     },
     logo:{
       alignItems:'center',
@@ -38,8 +30,88 @@ var styles = StyleSheet.create({
         textAlign: 'center',
         color: '#333333',
         marginBottom: 5,
-    },
+    }
 });
+
+var  textStyles =StyleSheet.create({
+  col: {
+    flex: 1,
+    flexDirection: 'column',
+    // alignItems: 'center', // this will prevent TFs from stretching horizontal
+    marginLeft: 7, marginRight: 7,
+    // backgroundColor: MKColor.Lime,
+  },
+  textfield: {
+    height: 28,  // have to do it on iOS
+    marginTop: 22,
+  },
+  textfieldWithFloatingLabel: {
+    height: 38,  // have to do it on iOS
+    marginTop: 10,
+  },
+});
+
+const Textfield = MKTextField.textfield()
+  .withPlaceholder('Text...')
+  .withStyle(styles.textfield)
+  .build();
+
+const TextfieldWithFloatingLabel = MKTextField.textfieldWithFloatingLabel()
+  .withPlaceholder('Number...')
+  .withStyle(styles.textfieldWithFloatingLabel)
+  .withFloatingLabelFont({
+    fontSize: 10,
+    fontStyle: 'italic',
+    fontWeight: '200',
+  })
+  .withKeyboardType('numeric')
+  .build();
+
+const ColoredTextfield = mdl.Textfield.textfield()
+  .withPlaceholder('Text...')
+  .withStyle(styles.textfield)
+  .withTintColor(MKColor.Lime)
+  .withTextInputStyle({color: MKColor.Orange})
+  .build();
+
+const PasswordInput = mdl.Textfield.textfieldWithFloatingLabel()
+  .withPassword(true)
+  .withPlaceholder('Password')
+  .withDefaultValue('!123')
+  .withHighlightColor(MKColor.Lime)
+  .withStyle(styles.textfieldWithFloatingLabel)
+  .withOnFocus(() => console.log('Focus'))
+  .withOnBlur(() => console.log('Blur'))
+  .withOnEndEditing((e) => console.log('EndEditing', e.nativeEvent.text))
+  .withOnSubmitEditing((e) => console.log('SubmitEditing', e.nativeEvent.text))
+  .withOnTextChange((e) => console.log('TextChange', e))
+  .withOnChangeText((e) => console.log('ChangeText', e))
+  .build();
+
+class Login extends React.Component {
+    render(){
+        let Actions = this.props.routes;
+
+        return (
+            <View style={styles.container}>
+              <View style={textStyles.row}>
+                <View style={textStyles.col}>
+                  <ColoredTextfield/>
+                  <Text style={textStyles.legendLabel}>Textfield</Text>
+                </View>
+                <View style={textStyles.col}>
+                  <PasswordInput/>
+                  <Text style={textStyles.legendLabel}>With floating label</Text>
+                </View>
+              </View>
+                <Image style={styles.logo} source={require('../assets/logo.png')}/>
+                <Button onPress={Actions.pop}>Login</Button>
+            </View>
+        );
+    }
+}
+
+
 
 
 
