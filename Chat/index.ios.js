a49a9bdf4fc77ea5d36972575042b6d0b3494534
Anton Ivanov <an.ivan0209@gmail.com>
2016-03-29 04:23:26 +0300

    platform of react/react-native overwritten

---
+++ Chat/index.ios.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var React = require('react-native');
+var {AppRegistry, StyleSheet,Text,View} = React;
+var Launch = require('./components/Launch');
+var Register = require('./components/Register');
+var Login = require('./components/Login');
+var {Router, routerReducer, Route, Container, Animations, Schema} = require('react-native-redux-router');
+var {NavBar, NavBarModal} = require('./components/NavBar');
+var Error = require('./components/Error');
+var Home = require('./components/Home');
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux/native';
+import createLogger from 'redux-logger';
+
+const loggerMiddleWare = createLogger();
+
+const createStoreWithMiddleware = applyMiddleware(loggerMiddleWare)(createStore);
+const reducer = combineReducers({routerReducer});
+let store = createStoreWithMiddleware(reducer);
+
+class App extends React.Component {
+    render(){
+        return (
+            <View style={{flex:1}}>
+                <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'#F5FCFF'}}/>
+                <Router>
+                    <Schema name="modal" sceneConfig={Animations.FlatFloatFromBottom} navBar={NavBarModal}/>
+                    <Schema name="default" sceneConfig={Animations.FlatFloatFromRight} navBar={NavBar}/>
+                    <Schema name="withoutAnimation" navBar={NavBar}/>
+                    <Schema name="tab" navBar={NavBar}/>
+
+                    <Route name="launch" component={Launch} initial={true} hideNavBar={true} title="Launch"/>
+                    <Route name="register" component={Register} title="Register"/>
+                    <Route name="home" component={Home} title="Home" type="replace"/>
+                    <Route name="login" component={Login} schema="modal"/>
+                    <Route name="register2" component={Register} schema="withoutAnimation"/>
+                    <Route name="error" component={Error} schema="popup"/>
+                </Router>
+
+            </View>
+        );
+    }
+}
+class Example extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                {() => <App />}
+            </Provider>
+        );
+    }
+}
+
+AppRegistry.registerComponent('Example', () => Example);
