badca0f3f678a1d642adcf8290fbffa8cd544ac9
Anton Ivanov <an.ivan0209@gmail.com>
2016-04-14 04:39:34 +0300

    new message view renovated

--- Chat/components/NewMessage.js
+++ Chat/components/NewMessage.js
@@ -1,64 +1,113 @@
 'use strict';
-
 var React = require('react-native');
+
 var {
-   AppRegistry,
-   StyleSheet,
-   Text,
-   TextInput,
-   View
- } = React;
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+} = React;
+var styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#005B7D',
+    alignItems: 'stretch',
+    paddingTop: 100,
+  },
+  scrollView :{
+    flex: 1,
+  },
+  contentContainerStyle: {
+    flex: 1,
+  },
+  inputcontainer: {
+    flex: .5,
+    backgroundColor: '#005B7D',
+    alignItems: 'center',
+    marginBottom: 3,
+    borderWidth:2,
+    borderColor:'#ff0000'
+  },
+  message: {
+    flex: .25,
+    alignItems: 'center',
+    paddingTop:10,
+  },
+  msgMenu: {
+    paddingTop:10,
+    paddingBottom:10,
+    paddingLeft:10,
+    borderBottomWidth:1,
+    borderColor:'#004058'
+  },
+  msgheader: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+  },
+  input: {
+    flex: 1,
+    width: 200,
+    fontSize:14,
+    fontWeight: 'bold',
+    paddingLeft:20,
+    borderWidth:2,
+    borderColor:'#ff0000',
+    color: '#ffffff',
+  },
+});
 
- var {KeyBoardResizeView} = require('react-native-sk-keyboard-responder');
+class NewMessage extends React.Component{
+  constructor() {
+    super();
 
- var NewMessage  = React.createClass({
-     getList: function(){
-       var list = [];
-       for (var i = 0; i < 20; i++) {
-         list.push(<Text key={i} style={styles.item}>{'Sample' + i}</Text>)
-       }
-       return list;
-     },
-     render: function(){
-     return (
-       <View style={styles.container}>
-        {this.getList()}
-        <KeyBoardResizeView style={styles.inputBox}>
-          <TextInput
-            style={styles.input}
-            placeholderTextColor='#AFAFAF'
-            placeholder='Placeholder'
-            />
-        </KeyBoardResizeView>
-       </View>
-     )
-   },
- });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView ref='scrollView' keyboardDismissMode='interactive' style={styles.scrollView}
+           contentContainerStyle={styles.contentContainerStyle}>
+          <View style={styles.msgMenu}>
+            <Text style={styles.msgheader}>Subject:</Text>
+          </View>
+          <View style={styles.msgMenu}>
+            <Text style={styles.msgheader}>To:</Text>
+          </View>
+          <View style={styles.msgMenu}>
+            <Text style={styles.msgheader}>When to Send:</Text>
+          </View>
+
+          <View ref='firstname' style={styles.inputcontainer}>
+            <TextInput
+              style={styles.input}
+              placeholder='First Name'
+              returnKeyType='next'
+              onFocus={this.inputFocused.bind(this,'firstname')}></TextInput>
+          </View>
 
 
- const styles = StyleSheet.create({
-    container: {
-     //  flex: 1,
-      backgroundColor: 'grey',
-    },
-    listBox: {
-      flex: 1,
-      justifyContent: 'space-around',
-    },
-    item: {
-      fontSize: 17,
-    },
-    inputBox: {
-      height: 50,
-      margin: 5,
-      borderWidth:1,
-      borderColor:'#E1E1E1',
-      borderRadius: 5,
-      backgroundColor:'#FFF',
-    },
-    input: {
-     flex: 1,
-     fontSize:14,
-    }
-  });
+          <View style={styles.message}>
+            <Text style={styles.msgheader}>Is there a text input above?</Text>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+  inputFocused(refName){
+    setTimeout(() => {
+      let scrollResponder = this.refs.scrollView.getScrollResponder();
+      scrollResponder.scrollResponderScrollNativeHandleToKeyboard(
+        React.findNodeHandle(this.refs[refName]),
+        110,
+        true
+      );
+    }, 50);
+  }
+}
+
+
+
 module.exports = NewMessage;
